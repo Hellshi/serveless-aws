504dd58ddd6d694266a4ed5c852d06e9895bfba9
Hellshi <hcslimaa@gmail.com>
2023-04-13 19:58:58 -0300

    fix: id generation

--- 04-dynamo-db/trigger-dynamo-db/src/heroesInsert.js
+++ 04-dynamo-db/trigger-dynamo-db/src/heroesInsert.js
@@ -1,5 +1,7 @@
-const { randomUUID: uuid } = require('crypto')
+const { v4: uuid } = require('uuid')
+const Joi = require('@hapi/joi')
 const AWS = require('aws-sdk')
+const decoratorValidator = require('./util/decoratorValidator')
 class Handler {
     constructor({ dynamoDbSvc }) {
       this.dynamoDbSvc = dynamoDbSvc
@@ -9,6 +11,13 @@ class Handler {
     async insertItem(params) {
       return this.dynamoDbSvc.put(params).promise()
     }
+
+    static validator() {
+      return Joi.object({
+          nome: Joi.string().max(100).min(2).required(),
+          poder: Joi.string().max(20).required()
+      })
+  }
 
     prepareData(data) {
       const params = {
@@ -40,15 +49,14 @@ class Handler {
 
     async main(event) {
         try {
-        const data = JSON.parse(event.body)
+        const data = event.body
         const dbParams = this.prepareData(data)
         await this.insertItem(dbParams)
 
         return this.handleSuccess(dbParams.Item)
           
         } catch(error) {
-
-            console.log('Error***', error.stack)
+            console.log('Error***', error)
 
             return this.handleError({ statusCode: 500 })
         }
@@ -60,4 +68,8 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient()
 const handler = new Handler({
   dynamoDbSvc: dynamoDb
 })
-module.exports = handler.main.bind(handler)
+module.exports =  decoratorValidator(
+  handler.main.bind(handler),
+  Handler.validator(),
+  'body'
+) 
