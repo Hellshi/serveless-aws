38ce0c48b5a5402ff9ef1ed72e76f797d56d3b5b
Hellshi <hcslimaa@gmail.com>
2023-04-18 08:02:29 -0300

    feat: set up to mememaker

---
+++ 06-layers/layers-node-js-modules/mememaker/mememaker/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -rf dependencies
+mkdir -p dependencies
+
+docker run --rm \
+    -v "$PWD"/dependencies:/lambda/opt \
+    lambci/yumda:2 yum install -y git GraphicsMagick
+
+cd dependencies
+zip -yr ../dependencies.zip .
+cd ..
+# To test:
+
+# sam local invoke --no-event
+
+# OR
+
+# sls invoke local --docker -f hello-world
+
+# To prepare to deploy:
+
+# cd dependencies
+# zip -yr ../dependencies.zip .
+# cd ..
