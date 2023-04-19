762c13e1801c27d2176cfa4b658dd1817084ab6c
Hellshi <hcslimaa@gmail.com>
2023-04-19 14:44:58 -0300

    fix: docker compose to ensure req to local aws works

--- 07-tooling/tooling-s3-sqs/index.js
+++ 07-tooling/tooling-s3-sqs/index.js
@@ -1,30 +1,27 @@
 'use-strict'
 const AWS = require('aws-sdk')
 
-const host = process.env.LOCALSTACK_HOST || "localhost"
-const s3port = process.env.S3_PORT || "4572"
+const host = process.env.LOCALSTACK_HOST || "localstack"
+const s3port = process.env.S3_PORT || "4566"
 const s3config = {
   s3ForcePathStyle: true,
   endpoint: new AWS.Endpoint(
-    `http://localhost`
+    `http://${host}:${s3port}`
   )
 }
-const S3 = new AWS.S3({
-  endpoint: `http://localstack:4572`
-})
-
-
+const S3 = new AWS.S3(s3config)
 module.exports.handler = async (event) => {
   const allBuckets = await S3.listBuckets().promise().catch((e) => {
-    console.log(S3.endpoint)
+    console.log(e)
   })
+
   console.log(allBuckets)
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        allBuckets: "JSON.parse(allBuckets)"
+        allBuckets
       },
       null,
       2
