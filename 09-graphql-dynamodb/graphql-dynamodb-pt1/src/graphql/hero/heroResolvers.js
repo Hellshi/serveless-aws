833444dfb2f8956c8e700091d61391be21b0efbe
Hellshi <hcslimaa@gmail.com>
2023-04-25 11:56:22 -0300

    feat: graphql works with node 14 and adds mutation

--- 09-graphql-dynamodb/graphql-dynamodb-pt1/src/graphql/hero/heroResolvers.js
+++ 09-graphql-dynamodb/graphql-dynamodb-pt1/src/graphql/hero/heroResolvers.js
@@ -1,13 +1,18 @@
 const resolvers = {
     Query: {
         async getHero(root, args, context, info) {
-            return 'Hello World'
+            console.log({
+                args
+            })
+
+            return context.Hero.findAll(args)
         }
     },
     // POST (att, register, remove)
     Mutation: {
         async createHero(root, args, context, info) {
-            return 'Hello World!'
+            const { id } = await context.Hero.create(args)
+            return id
         }
     }
 }
