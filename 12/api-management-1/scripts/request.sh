0df4f74500e2c7bfcaf279d76754a90405845f1a
Hellshi <hcslimaa@gmail.com>
2023-04-26 09:54:49 -0300

    feat: serverless plan based limit usage

---
+++ 12/api-management-1/scripts/request.sh
@@ -0,0 +1,13 @@
+HOST=http://localhost:3000
+APIKEY="d41d8cd98f00b204e9800998ecf8427e"
+curl --silent \
+    -H "x-api-key: $APIKEY" \
+    $HOST/dev/hello
+
+#This is generated on sls-deploy but can be accessed in dashboard
+#test@test.com: kapDS0TdUF5Buu1N4Bljj7ie5p3Am0ZM6UwdrLbF
+#hell@test.com: PD83cWatbw1YXFhpM6hJ3852TBa1ZNLy1Az9gGGX
+#bibia@test.com: jK2uzIe9EZ4jUNqHOQiak69mcOeRR0pA2RKB8J2c
+#myPaidKey: ckQoHrfwDW2R4sNdCEknc8bMhaR40TYN34T4vOaq
+#hellemsantos@gmail.com: 02nFwQCMXu2ClaOzRtpfg4kuV5S7QXeO8AvZUOie
+#endpoint: GET - https://gmiapyq55h.execute-api.us-east-1.amazonaws.com/dev/hello
