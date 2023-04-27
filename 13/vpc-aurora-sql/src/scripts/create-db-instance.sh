2663af6f48ceb09818d9dc92a21781311793673f
Hellshi <hcslimaa@gmail.com>
2023-04-27 11:07:32 -0300

    feat: debug database creation later

--- 13/vpc-aurora-sql/src/scripts/create-db-instance.sh
+++ 13/vpc-aurora-sql/src/scripts/create-db-instance.sh
@@ -5,6 +5,7 @@ DB_NAME=heroes
 SECRET_NAME=aourora-secret01
 
 RESOURCE_ARN=arn:aws:rds:us-east-1:243886104914:cluster:serverless-course
+SECRET_ARN=arn:aws:secretsmanager:us-east-1:243886104914:secret:aourora-secret02-xsuqCS
 
 aws rds create-db-cluster \
     --engine-version 5.6.10a \
@@ -21,7 +22,7 @@ aws rds create-db-cluster \
 CREATING="creating"
 STATUS=$CREATING
 
-while [ $STATUS==$CREATING ]
+while (( $STATUS == $CREATING ))
 do
     STATUS=$(aws rds describe-db-clusters \
         --db-cluster-identifier $CLUSTER_NAME \
@@ -31,3 +32,40 @@ do
     echo $STATUS
     sleep 1
 done
+
+#This is not working properly 
+# TODO: Debug later
+aws secretsmanager create-secret \
+    --name $SECRET_NAME \
+    --description "Credentials for aurora serverless db" \
+    --secret-string '{"username": "'$USERNAME'", "password": "'$PASSWORD'"}' \
+    --region us-east-1 \
+    | tee secret.txt
+
+aws rds-data execute-statement \
+    --resource-arn $RESOURCE_ARN \
+    --secret-arn $SECRET_ARN \
+    --database mysql \
+    --sql "show databases;" \
+    --region us-east-1 \
+    | tee cmd-show-dbs.txt
+
+aws rds-data execute-statement \
+    --resource-arn $RESOURCE_ARN \
+    --secret-arn $SECRET_ARN \
+    --database mysql \
+    --sql "CREATE DATABASE $DB_NAME;" \
+    --region us-east-1 \
+    | tee cmd-create-db.txt
+
+aws rds describe-db-subnet-groups \
+    | tee db-subnets.txt
+
+aws secretsmanager delete-secret \
+    --secret-id $SECRET_NAME \
+    | tee secret-delete.txt
+
+aws rds delete-db-cluster \
+    --db-cluster-identifier $CLUSTER_NAME \
+    --skip-final-snapshot \
+    | tee rds-delete.cluster.txt
