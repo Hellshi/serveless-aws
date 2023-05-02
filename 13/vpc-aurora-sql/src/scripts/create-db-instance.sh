414371d0b766842b1e63429b69b16be6b9603f2e
Hellshi <hcslimaa@gmail.com>
2023-05-02 10:21:04 -0300

    feat: ensure database is accessible

--- 13/vpc-aurora-sql/src/scripts/create-db-instance.sh
+++ 13/vpc-aurora-sql/src/scripts/create-db-instance.sh
@@ -1,11 +1,15 @@
 # required variables
-CLUSTER_NAME=
-USERNAME=
+CLUSTER_NAME=cluster-updated-version
+DB_NAME=heroes
+USERNAME=hellshi
+
 PASSWORD=
-DB_NAME=
 SECRET_NAME=
 RESOURCE_ARN=
 SECRET_ARN=
+
+# Create database
+#I've tried to update engine-version. However it did not went rigth 
 aws rds create-db-cluster \
     --engine-version 5.6.10a \
     --db-cluster-identifier $CLUSTER_NAME \
@@ -21,6 +25,8 @@ aws rds create-db-cluster \
 CREATING="creating"
 STATUS=$CREATING
 
+#This does not work due "==".
+# OS: Linux Ubuntu 20.04.06 running in wsl 1.2.5.0
 while [ $STATUS == $CREATING ]
 do
     STATUS=$(aws rds describe-db-clusters \
@@ -37,7 +43,7 @@ aws secretsmanager create-secret \
     --description "Credentials for aurora serverless db" \
     --secret-string '{"username": "'$USERNAME'", "password": "'$PASSWORD'"}' \
     --region us-east-1 \
-    | tee secret.txt
+    | tee rds-secret.txt
 
 aws rds-data execute-statement \
     --resource-arn $RESOURCE_ARN \
@@ -56,7 +62,7 @@ aws rds-data execute-statement \
     | tee cmd-create-db.txt
 
 aws rds describe-db-subnet-groups \
-    | tee db-subnets.txt
+    | tee rds-db-subnets.txt
 
 aws secretsmanager delete-secret \
     --secret-id $SECRET_NAME \
